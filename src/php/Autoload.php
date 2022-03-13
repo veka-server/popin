aa73bfc56f7c1294da7c76ed1eec3d85f3f3f987
veka-server <veka610@gmail.com>
2022-03-13 14:19:15 +0100

    init

---
+++ src/php/Autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace VekaServer\Popin;
+
+class Autoload
+{
+
+    public static function getPathView(){
+        return [];
+    }
+
+    public static function getPathJS(){
+        return dirname(__DIR__).DIRECTORY_SEPARATOR.'js';
+    }
+
+    public static function getPathCSS(){
+        return dirname(__DIR__).DIRECTORY_SEPARATOR.'css';
+    }
+
+}

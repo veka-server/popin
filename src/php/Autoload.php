cfafeeabb8524bd09f74e2839e7fca421cce5c3b
Veka <veka610@gmail.com>
2022-03-16 21:25:30 +0100

    Update Autoload.php

--- src/php/Autoload.php
+++ src/php/Autoload.php
@@ -10,16 +10,16 @@ class Autoload extends PluginAbstract
     protected static function getAllRequiredPlugin():array {
         return [\VekaServer\Jquery\Autoload::class, \VekaServer\FontAwesome\Autoload::class];
     }
-    
-    public static function getPathView():array {
+
+    public static function getVIEW():array {
         return [];
     }
 
-    public static function getPathJS():array {
+    public static function getJS():array {
         return [dirname(__DIR__).DIRECTORY_SEPARATOR.'js'];
     }
 
-    public static function getPathCSS():array {
+    public static function getCSS():array {
         return [dirname(__DIR__).DIRECTORY_SEPARATOR.'css'];
     }
 
